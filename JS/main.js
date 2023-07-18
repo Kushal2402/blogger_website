d9884fb3e3fd8ace3a38c49c7db8d55074f78e86
Kushal2402 <itsmekushaljaiswal2017@gmail.com>
2023-07-18 16:09:41 +0530

    initial commit

---
+++ JS/main.js
@@ -0,0 +1,54 @@
+const responsive={
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+}
+
+
+
+$(document).ready(function()
+    {
+        $nav = $('.nav');
+        $toggleCollapse =$('.toggle-collapse');
+
+        /**  click event on toggle menu**/
+        $toggleCollapse.click(function()
+        {
+            $nav.toggleClass('collapse');
+
+        })
+
+        //owl-carousel for blog
+        $('.owl-carousel').owlCarousel({
+
+            loop:true,
+            autoplay:true,
+            autoplayTimeout:3000,
+            dots:false,
+            nav:true,
+            navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+            responsive:responsive
+
+        })
+
+      $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop:0
+        },1000);
+      })
+
+      ///aos instance
+
+      AOS.init();
+
+    
+});
